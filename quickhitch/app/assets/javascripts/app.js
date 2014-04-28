3e3bd91367591370bd61cd652c317698a692d256
Alexa <alexacgreenberg@gmail.com>
2014-04-28 16:42:56 -0400

    further integration

--- quickhitch/app/assets/javascripts/app.js
+++ quickhitch/app/assets/javascripts/app.js
@@ -3,7 +3,7 @@ $(function() {
 	$("button#conference").on("click", createConference);
 
 	function findRide() {
-		$('#description').hide();
+		$('#description, #buttons').hide();
 		$('#container').animate({margin:'4% 20% 0 20%'}, 800);
 		$('#container').append('<div id="riderInput">Desired Pickup Time<br/><input type="text" name="time"><br/><br/>' +
 			'Conference Code<br/><input type="text" name="confcode"><br/><br/>' +
@@ -22,22 +22,23 @@ $(function() {
 	}
 
 	function createConference() {
-		$('#description').hide();
+		$('#description, #buttons').hide();
 		$('#container').animate({margin:'4% 20% 0 20%'}, 800);
-		$('#container').append('<div id="confInput">Conference Name<br/><input type="text" name="name"><br/><br/>' +
-			'Date(s)<br/><input type="text" name="date"><br/><br/>' +
-			'Location<br/><input type="text" name="location"><br/><br/>' +
-			'<input type="submit" class="search" value="Add to QuickHitch!"></input></div>');
-		$(".search").on("click", function() {
-			var name = $("input[name='name']").val();
-			if (name) {
-				var code = name.match(/\b(\w)/g).join('') + $("input[name='date']").val();
-				$("#confInput").remove();
-				$('#container').append('<div id="confResults">Your conference has been added!<br/><br/>' +
-					'Give this code to conference-goers, <br/>so that they can use it to find a ride: <br/><span class="code">' + 
-					 code + '</span></div>');
-			}
-		});
+		$('#user-info, #conference-form').css('display','block');
+		// $('#container').append('<div id="confInput">Conference Name<br/><input type="text" name="name"><br/><br/>' +
+		// 	'Date(s)<br/><input type="text" name="date"><br/><br/>' +
+		// 	'Location<br/><input type="text" name="location"><br/><br/>' +
+		// 	'<input type="submit" class="search" value="Add to QuickHitch!"></input></div>');
+		// $(".search").on("click", function() {
+		// 	var name = $("input[name='name']").val();
+		// 	if (name) {
+		// 		var code = name.match(/\b(\w)/g).join('') + $("input[name='date']").val();
+		// 		$("#confInput").remove();
+		// 		$('#container').append('<div id="confResults">Your conference has been added!<br/><br/>' +
+		// 			'Give this code to conference-goers, <br/>so that they can use it to find a ride: <br/><span class="code">' + 
+		// 			 code + '</span></div>');
+		// 	}
+		// });
 	}
 
 	$("#logo").on("click", function() {
